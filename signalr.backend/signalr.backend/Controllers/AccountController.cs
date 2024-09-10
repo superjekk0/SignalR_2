e2ea16caae5310492dcf4343c72bdd32628ed625
Mathieu Briau <mathieu.briau@gmail.com>
2024-09-10 13:00:42 -0400

    WIP utilisation de tokens

--- signalr.backend/signalr.backend/Controllers/AccountController.cs
+++ signalr.backend/signalr.backend/Controllers/AccountController.cs
@@ -1,11 +1,12 @@
 ﻿using signalr.backend.Data;
 using signalr.backend.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace signalr.backend.Controllers
 {
@@ -52,16 +53,32 @@ namespace signalr.backend.Controllers
             var result = await SignInManager.PasswordSignInAsync(login.Email, login.Password, true, lockoutOnFailure: false);
             if (result.Succeeded)
             {
-                return Ok(new LoginResultDTO() { Email = login.Email });
+                Claim? nameIdentifierClaim = User.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+                // Note: On ajoute simplement le NameIdentifier dans les claims. Il n'y aura pas de rôle pour les utilisateurs du WebAPI.
+                List<Claim> authClaims = new List<Claim>();
+                authClaims.Add(nameIdentifierClaim);
+
+                SymmetricSecurityKey signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("C'est tellement la meilleure cle qui a jamais ete cree dans l'histoire de l'humanite (doit etre longue)"));
+
+                string issuer = this.Request.Scheme + "://" + this.Request.Host;
+
+                DateTime expirationTime = DateTime.Now.AddMinutes(30);
+
+                JwtSecurityToken token = new JwtSecurityToken(
+                    issuer: issuer,
+                    audience: null,
+                    claims: authClaims,
+                    expires: expirationTime,
+                    signingCredentials: new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256Signature)
+                );
+
+                string tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+                return Ok(new LoginResultDTO() { Email = login.Email, Token = tokenString });
             }
 
             return NotFound(new { Error = "L'utilisateur est introuvable ou le mot de passe de concorde pas" });
-        }
-
-        public async Task<ActionResult> Logout()
-        {
-            await SignInManager.SignOutAsync();
-            return Ok();
         }
 
         [Authorize]
--- signar.ng/src/app/services/authentication.service.ts
+++ signar.ng/src/app/services/authentication.service.ts
@@ -13,41 +13,42 @@ export class AuthenticationService {
   baseUrl = "https://localhost:7060/api/";
   accountBaseUrl = this.baseUrl + "Account/";
 
-  authenticatedUserEmail : string | null = null;
+  authenticatedUserEmail: string | null = null;
 
   constructor(public http: HttpClient) {
     this.authenticatedUserEmail = localStorage.getItem(LOCAL_STORAGE_EMAIL_KEY);
   }
 
-  async registerAndLogin(registerData:RegisterDTO) : Promise<void> {
+  async registerAndLogin(registerData: RegisterDTO): Promise<void> {
     let result = await lastValueFrom(this.http.post<LoginResultDTO>(this.accountBaseUrl + 'Register', registerData));
     this.setUserEmail(result.email);
   }
 
-  async login(loginData:LoginDTO) : Promise<void> {
+  async login(loginData: LoginDTO): Promise<void> {
     let result = await lastValueFrom(this.http.post<LoginResultDTO>(this.accountBaseUrl + 'Login', loginData));
+    localStorage.setItem("token", result.token);
     this.setUserEmail(result.email);
   }
 
-  async logout() : Promise<void> {
-    await lastValueFrom(this.http.get<any>(this.accountBaseUrl + 'Logout'));
+  async logout(): Promise<void> {
+    localStorage.removeItem("token");
     this.setUserEmail(null);
   }
 
-  async testAuthorize() : Promise<void> {
+  async testAuthorize(): Promise<void> {
     let result = await lastValueFrom(this.http.get<any>(this.accountBaseUrl + 'Test'));
     console.log(result);
   }
 
-  isLoggedIn() : boolean{
+  isLoggedIn(): boolean {
     return localStorage.getItem(LOCAL_STORAGE_EMAIL_KEY) != null;
   }
 
-  private setUserEmail(email:string|null){
-    if(email == null){
+  private setUserEmail(email: string | null) {
+    if (email == null) {
       localStorage.removeItem(LOCAL_STORAGE_EMAIL_KEY);
     }
-    else{
+    else {
       localStorage.setItem(LOCAL_STORAGE_EMAIL_KEY, email);
     }
     this.authenticatedUserEmail = email;
